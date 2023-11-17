Chia-Network/chia-blockchain

Update key functions to handle observer only functionality

tests/wallet/test_wallet_node.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import pytest
-from blspy import PrivateKey
+from blspy import G1Element, PrivateKey
 
 from chia.protocols.wallet_protocol import CoinState
 from chia.simulator.block_tools import test_constants
@@ -75,7 +75,6 @@ async def test_get_private_key_missing_key(
     key = await node.get_key(fingerprint)
 
     assert key is None
-    assert isinstance(key, PrivateKey)
 
 
 @pytest.mark.asyncio
@@ -98,6 +97,91 @@ async def test_get_private_key_missing_key_use_default(
     assert key is not None
     assert isinstance(key, PrivateKey)
     assert key.get_g1().get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+async def test_get_public_key(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    key = await node.get_key(fingerprint, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+async def test_get_public_key_default_key(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    # Add a couple more keys
+    keychain.add_public_key(
+        "83062a1b26d27820600eac4e31c1a890a6ba026b28bb96bb66454e9ce1033f4cba8824259dc17dc3b643ab1003e6b961"
+    )
+    keychain.add_public_key(
+        "a272d5aaa6046e64bd7fd69bae288b9f9e5622c13058ec7d1b85e3d4d1acfa5d63d6542336c7b24d2fceab991919e989"
+    )
+
+    # When no fingerprint is provided, we should get the default (first) key
+    key = await node.get_key(None, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("fingerprint", [None, 1234567890])
+async def test_get_public_key_missing_key(
+    root_path_populated_with_config: Path, get_temp_keyring: Keychain, fingerprint: Optional[int]
+) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring  # empty keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+
+    # Keyring is empty, so requesting a key by fingerprint or None should return None
+    key = await node.get_key(fingerprint, private=False)
+
+    assert key is None
+
+
+@pytest.mark.asyncio
+async def test_get_public_key_missing_key_use_default(
+    root_path_populated_with_config: Path, get_temp_keyring: Keychain
+) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    # Stupid sanity check that the fingerprint we're going to use isn't actually in the keychain
+    assert fingerprint != 1234567890
+
+    # When fingerprint is provided and the key is missing, we should get the default (first) key
+    key = await node.get_key(1234567890, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
 
 
 def test_log_in(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None:

wokurka7/chia

tests/wallet/test_wallet_node.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import pytest
-from chia_rs import PrivateKey
+from chia_rs import G1Element, PrivateKey
 
 from chia.protocols import wallet_protocol
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
@@ -84,7 +84,6 @@ async def test_get_private_key_missing_key(
     key = await node.get_key(fingerprint)
 
     assert key is None
-    assert isinstance(key, PrivateKey)
 
 
 @pytest.mark.anyio
@@ -107,6 +106,91 @@ async def test_get_private_key_missing_key_use_default(
     assert key is not None
     assert isinstance(key, PrivateKey)
     assert key.get_g1().get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+async def test_get_public_key(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    key = await node.get_key(fingerprint, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+async def test_get_public_key_default_key(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    # Add a couple more keys
+    keychain.add_public_key(
+        "83062a1b26d27820600eac4e31c1a890a6ba026b28bb96bb66454e9ce1033f4cba8824259dc17dc3b643ab1003e6b961"
+    )
+    keychain.add_public_key(
+        "a272d5aaa6046e64bd7fd69bae288b9f9e5622c13058ec7d1b85e3d4d1acfa5d63d6542336c7b24d2fceab991919e989"
+    )
+
+    # When no fingerprint is provided, we should get the default (first) key
+    key = await node.get_key(None, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("fingerprint", [None, 1234567890])
+async def test_get_public_key_missing_key(
+    root_path_populated_with_config: Path, get_temp_keyring: Keychain, fingerprint: Optional[int]
+) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring  # empty keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+
+    # Keyring is empty, so requesting a key by fingerprint or None should return None
+    key = await node.get_key(fingerprint, private=False)
+
+    assert key is None
+
+
+@pytest.mark.asyncio
+async def test_get_public_key_missing_key_use_default(
+    root_path_populated_with_config: Path, get_temp_keyring: Keychain
+) -> None:
+    root_path: Path = root_path_populated_with_config
+    keychain: Keychain = get_temp_keyring
+    config: Dict[str, Any] = load_config(root_path, "config.yaml", "wallet")
+    node: WalletNode = WalletNode(config, root_path, test_constants, keychain)
+    pk: G1Element = keychain.add_public_key(
+        "c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+    fingerprint: int = pk.get_fingerprint()
+
+    # Stupid sanity check that the fingerprint we're going to use isn't actually in the keychain
+    assert fingerprint != 1234567890
+
+    # When fingerprint is provided and the key is missing, we should get the default (first) key
+    key = await node.get_key(1234567890, private=False)
+
+    assert key is not None
+    assert isinstance(key, G1Element)
+    assert key.get_fingerprint() == fingerprint
 
 
 def test_log_in(root_path_populated_with_config: Path, get_temp_keyring: Keychain) -> None: